21cbf8d4fd78ba112f51edc3fb444060ed1a53c5
Ricardo Martinez <rbeltra1@mycambrian.ca>
2025-05-24 18:32:13 -0400

    Commit without BMI and weather buttons

--- main.py
+++ main.py
@@ -6,9 +6,13 @@ app=Flask(__name__, template_folder='templates')
 def index():
     return render_template("index.html")
 
-@app.route('/experience')
-def experience():
-    return render_template("experience.html")
+@app.route('/weather')
+def weather():
+    return render_template("weather.html")
+
+@app.route('/bmi')
+def bmi():
+    return render_template("bmi.html")
 
 if __name__=='__main__':
     app.run(host='127.0.0.1', debug=True)
